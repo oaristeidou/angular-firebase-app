14a923a4e5c47bfdf50d263df7fded8a5b457c5e
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-12-16 23:50:34 +0100

    - Add firebase config

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,7 +1,12 @@
 
 
 export const firebaseConfig = {
-
+  apiKey: "AIzaSyAGFltFl0QaDbkdJn1vgros8AV50wDIHy4",
+  authDomain: "angular-firebase-app-29ee9.firebaseapp.com",
+  databaseURL: "https://angular-firebase-app-29ee9.firebaseio.com",
+  projectId: "angular-firebase-app-29ee9",
+  storageBucket: "angular-firebase-app-29ee9.appspot.com",
+  messagingSenderId: "975029687258"
 };
 
 
