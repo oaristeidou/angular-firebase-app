01a1f5e05f7d02c2993820e50b3a4fcc9a05a2f7
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-12-17 14:35:45 +0100

    - Using firebase cloud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,15 @@ export class AppComponent {
 
   constructor(private db : AngularFirestore){
     this.courses = db.collection("courses");
+    db.persistenceEnabled$.subscribe(
+      value => console.log(value)
+    )
+
+    const collection: AngularFirestoreCollection<any> = db.collection('courses');
+
+// Notice how the observable is separated from write options
+    const collection$: Observable<any> = collection.valueChanges();
+    collection$.subscribe(data => console.log(data))
 
   }
 }
