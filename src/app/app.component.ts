4211ca9b7ac410bb1a22c3fd796be5e7c8afcb82
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-12-18 20:56:11 +0100

    - Develop a list remove button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
 
   courses$: AngularFireList<any[]>;
   lesson$: AngularFireObject<any>;
+  firstCourse: any;
 
   constructor(db: AngularFireDatabase) {
 
@@ -23,13 +24,26 @@ export class AppComponent {
     this.lesson$ = db.object('lessons/-L0X0Duk2oY_2GefTket');
 
     this.lesson$.valueChanges().subscribe(console.log);
+
+    this.courses$.valueChanges().map(courses => courses[0])
+      .subscribe(
+        course => this.firstCourse=course.keys().next()
+      )
   }
 
   listPush(){
-    this.courses$.push({description: 'Test new course'} as any);
+    this.courses$.push({description: 'Test new course'} as any)
+      .then(
+        () => console.log("Push new course!!!")
+        , console.error
+      );
   }
 
   listRemove(){
+    this.courses$.remove(this.firstCourse)
+      .then(
+        ()=> console.log("Remove the first course!!!")
+        , console.error);
 
   }
 
