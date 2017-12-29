17d60692111584489ee0b4040f82434c827208fe
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-12-29 01:09:02 +0100

    - Find the lesson details by url

--- src/app/lesson-details/lesson-details.component.ts
+++ src/app/lesson-details/lesson-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {LessonsService} from "../shared/model/lessons.service";
+import {Lesson} from "../shared/model/lesson";
 
 @Component({
   selector: 'app-lesson-details',
@@ -7,13 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonDetailsComponent implements OnInit {
 
+  lesson: Lesson;
 
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private lessonService: LessonsService) {
   }
 
+
+  ngOnInit() {
+    const lessonUrl = this.route.snapshot.params['id'];
+    const lesson$ = this.lessonService.findLessonByUrl(lessonUrl)
+      .subscribe(lesson => this.lesson = lesson);
+  }
 
 
 }
--- src/app/shared/model/lessons.service.ts
+++ src/app/shared/model/lessons.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {Lesson} from "./lesson";
 import {AngularFireDatabase} from "angularfire2/database";
@@ -6,11 +6,18 @@ import {AngularFireDatabase} from "angularfire2/database";
 @Injectable()
 export class LessonsService {
 
-  constructor(private angularFireDb : AngularFireDatabase) { }
+  constructor(private angularFireDb: AngularFireDatabase) {
+  }
 
-  findAllLessons(): Observable<Lesson[]>{
+  findAllLessons(): Observable<Lesson[]> {
     return this.angularFireDb.list('lessons').snapshotChanges()
       .do(console.log)
       .map(Lesson.fromJsonToList);
+  }
+
+  findLessonByUrl(lessonUrl: string): Observable<Lesson> {
+    return this.angularFireDb.list('lessons', ref =>
+      ref.orderByChild('url').equalTo(lessonUrl)).snapshotChanges()
+      .map(results => Lesson.fromJson(results[0]));
   }
 }
