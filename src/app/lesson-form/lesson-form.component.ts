c0bed60152b60cb3beb85633db0ff76a167829cf
Odyssefs Aristeidou <airoddyisback@gmail.com>
2018-01-01 03:03:11 +0100

    - Develop a lesson form

--- src/app/lesson-form/lesson-form.component.ts
+++ src/app/lesson-form/lesson-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-lesson-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonFormComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form= this.formBuilder.group({
+      description: ['',Validators.required],
+      url: ['',Validators.required],
+      videoUrl: ['',[Validators.required]],
+      tags: ['',Validators.required],
+      longDescription: ['']
+    })
+  }
+
+  isErrorVisible(field:string, error:string) {
+    return this.form.controls[field].dirty
+      && this.form.controls[field].errors &&
+      this.form.controls[field].errors[error];
   }
 
 }
